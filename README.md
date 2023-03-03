340ed8f5168313d9c03aed7a2ec281f579663e86
henkin <talktopaul@gmail.com>
2023-03-03 05:32:20 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-sqrsam
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-sqrsam)
---
+++ src/App.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import 'bootswatch/dist/slate/bootstrap.min.css';
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+  import { Button, Container, Modal, Card } from 'sveltestrap';
+  let isOpen = false;
+  const toggle = () => (isOpen = !isOpen);
+</script>
+
+<main>
+  <Container fluid>
+    <h3 class="bg-light">.container-fluid</h3>
+    <div class="row">
+      <Card  class="col-5">
+        <img src="https://i.ytimg.com/vi/NUJIRujygvY/hqdefault.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">An item</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+        </ul>
+        <div class="card-body">
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+          <Button color="primary" on:click={toggle}>Hello World!</Button>
+          <Modal body {isOpen} {toggle} header="Hello World!">
+            <p>There's a song that we're singing. Come on</p>
+            <img
+              src="https://i.ytimg.com/vi/NUJIRujygvY/hqdefault.jpg"
+              alt="Come on Get Happy"
+              class="img-fluid"
+            />
+          </Modal>
+      </Card>
+      
+
+
+    </div>
+
+  </Container>
+
+</main>
+
+<style>
+
+</style>
